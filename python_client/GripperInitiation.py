de87822fc0ec69d139f3a74b255eb58f1d66a469
Vicon-OEM <brycecday23@gmail.com>
2025-06-16 12:41:43 -0500

    Fixed so that it actually works

--- python_client/GripperInitiation.py
+++ python_client/GripperInitiation.py
@@ -1,3 +1,5 @@
+import math
+from time import sleep
 from onrobot_sg import SG
 from iiwaPy3 import iiwaPy3
 from MATLABToolBoxStart import MATLABToolBoxStart
@@ -13,9 +15,47 @@ try:
 
 
     wakeup = MATLABToolBoxStart(KUKA_IP, KUKA_PORT)
+
+    try:
+        print(wakeup.start_client())
+        sleep(2)
+    except Exception as e:
+        print(f"Starting client failed with error message: {e}")
+    # Connect to the robot
+
     iiwa = iiwaPy3(KUKA_IP)
 
+
+    def go_home():
+        home = [0, 0, 0, 0, 0, 0, 0]
+        print("Moving to home pose")
+        iiwa.movePTPJointSpace(home, [0.5])
+
+
+    def reset_pose():
+        offset_axis_2_and_4 = math.radians(20)
+        offset_axis_4_and_6 = math.radians(-40)
+        vRel = [1.0]
+        loop_center_position = [
+            0,
+            offset_axis_2_and_4,
+            0,
+            offset_axis_2_and_4 + offset_axis_4_and_6 - math.radians(90),
+            0,
+            offset_axis_4_and_6,
+            0
+        ]
+        print("Moving to default pose")
+        iiwa.movePTPJointSpace(loop_center_position, vRel)
+
+
     curled = [0, -30, 0, -115, 0, 115, 0]
+    temp_list = []
+    for item in curled:
+        item = math.radians(item)
+        temp_list.append(item)
+    curled = temp_list
+
     iiwa.movePTPJointSpace(curled, [0.1])
 finally:
     iiwa.close()
